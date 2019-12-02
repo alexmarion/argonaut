5beacf57e2aadc83b1202e4dec71b9a5277f1954
alexmarion <alex@gotab.io>
2019-12-01 21:28:12 -0500

    working on recursive iterate

--- server/actions/grid.js
+++ server/actions/grid.js
@@ -1,4 +1,5 @@
-// TODO: I will probably kill this, just a late night idea
+/* eslint no-underscore-dangle: ["error", { "allow": ["_iterateRecurse"] }] */
+
 /**
  * @description The Grid is a specialized (potenitally nested) object built to act like an array.
  * The key feature of this class is that it lazy loads which allows for faster stringification.
@@ -33,6 +34,22 @@ class Grid {
   }
 
   /**
+   * @description Retreives all data as pure objects (unwraps grid).
+   * @returns {Object}
+   * @memberof Grid
+   */
+  getAll() {
+    if(this.dimensions.length === 1) {
+      return this.dimension;
+    }
+    return Object.keys(this.dimension).reduce((collapsedGrid, idx) => {
+      // eslint-disable-next-line no-param-reassign
+      collapsedGrid[idx] = this.dimension[idx].getAll();
+      return collapsedGrid;
+    }, {});
+  }
+
+  /**
    * @description Sets data in the grid.
    * @param {*} data
    * @param {...number} coordinates
@@ -54,26 +71,48 @@ class Grid {
     }
   }
 
-  getAll() {
-    if(this.dimensions.length === 1) {
-      return this.dimension;
-    }
-    return Object.keys(this.dimension).reduce((collapsedGrid, idx) => {
-      // eslint-disable-next-line no-param-reassign
-      collapsedGrid[idx] = this.dimension[idx].getAll();
-      return collapsedGrid;
-    }, {});
+  /**
+   * @description Iterates over each element in the grid.
+   * @param {Function} fxn - A function with the signature (data, ...coordinates)
+   * @memberof Grid
+   */
+  iterate(fxn) {
+    this._iterateRecurse(fxn);
+  }
+
+  _iterateRecurse(fxn, ...coordinates) {
+    const isLast = this.dimensions.length === 1;
+    Object.keys(this.dimension).forEach((coordinate) => {
+      if(isLast) {
+        fxn(this.dimension[coordinate], ...coordinates, coordinate);
+      } else {
+        this._iterateRecurse(fxn, ...coordinates, coordinate);
+      }
+    });
   }
 }
 
-const xMax = 100;
-const yMax = 100;
-const test = new Grid(xMax, yMax);
-for(let x = 0; x < xMax; x++) {
-  for(let y = 0; y < yMax; y++) {
-    test.set(`${x}${y}`, x, y);
+const dummyAgentObj = { type: 'agent', x: 'xxx', y: 'yyy' };
+const WORLD_WIDTH = 800;
+const WOLRD_HEIGHT = 600;
+
+// Empty grids
+const grid1 = JSON.stringify(Array(WORLD_WIDTH).fill(null)
+  .map(() => Array(WOLRD_HEIGHT).fill(null)));
+const grid2 = new Grid(WORLD_WIDTH, WOLRD_HEIGHT);
+
+// Filled grids
+const grid3 = JSON.stringify(Array(WORLD_WIDTH).fill(null)
+  .map(() => Array(WOLRD_HEIGHT).fill(dummyAgentObj)));
+const grid4 = new Grid(WORLD_WIDTH, WOLRD_HEIGHT);
+for(let x = 0; x < WORLD_WIDTH; x++) {
+  for(let y = 0; y < WOLRD_HEIGHT; y++) {
+    grid4.set(dummyAgentObj, x, y);
   }
 }
-console.log(test.getAll());
-// console.log(test.get(99, 50));
+console.log('grid1', grid1.length); // 4.803202 megabytes
+console.log('grid2', JSON.stringify(grid2.getAll()).length); // 2 bytes
+console.log('grid3', grid3.length); // 35.523202 megabytes
+console.log('grid4', JSON.stringify(grid4.getAll()).length); // 41.116582 megabytes
+
 module.exports = Grid;
